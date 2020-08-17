3030f4e9dc360436e3014f89096679de011561c1
Internet-Person-IP <taquiyt@gmail.com>
2020-08-17 21:30:41 +0200

    Updates

---
+++ precache-manifest.ff519d1f87e0ddedc92877a3d9ae9a3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c48c9b63f0713aff12da69f8baa499",
+    "url": "/pokeshop/index.html"
+  },
+  {
+    "revision": "35373c2defef6a4f79fa",
+    "url": "/pokeshop/static/css/main.ee4a2962.chunk.css"
+  },
+  {
+    "revision": "1fbfed082b677ae2f351",
+    "url": "/pokeshop/static/js/2.5b944e30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokeshop/static/js/2.5b944e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35373c2defef6a4f79fa",
+    "url": "/pokeshop/static/js/main.5124b4ae.chunk.js"
+  },
+  {
+    "revision": "0a7b3a85c5d853517d4a",
+    "url": "/pokeshop/static/js/runtime-main.1163a408.js"
+  },
+  {
+    "revision": "1e88999a7646e1c9e5dd1c7254b48b5e",
+    "url": "/pokeshop/static/media/PokeShop.1e88999a.png"
+  }
+]);
